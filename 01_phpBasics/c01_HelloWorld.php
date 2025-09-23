449181568b8bd264d237e53027d5625bf8b04192
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-23 15:00:33 -0500

    01_PHPFundamentals-01_VariablesDefinition

---
+++ 01_phpBasics/c01_HelloWorld.php
@@ -0,0 +1,22 @@
+<?php
+  echo "Hello, World!";
+
+  // This is a single-line comment
+  # This is also a single-line comment
+  /*
+    This is a multi-line comment
+    that spans multiple lines.
+  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> H1 tag <?php echo'additional hello world'?> </h1>
+</body>
+</html>
---
+++ 01_phpBasics/c02_variables.php
@@ -0,0 +1,26 @@
+<?php 
+
+  // strings
+  $name = 'Carlos';
+  echo $name;
+  echo "Hello $Carlos";
+  echo 'Hello $Carlos';
+
+  // integers
+  $age = 30;
+
+  // floats
+  $height = 1.75;
+
+  // booleans
+  $is_programmer = true;
+
+  //null
+  $adress;
+
+  // ***operations and methods***
+  echo 'Concat: ' . $name;
+
+  echo gettype($name); // string
+
+?>
