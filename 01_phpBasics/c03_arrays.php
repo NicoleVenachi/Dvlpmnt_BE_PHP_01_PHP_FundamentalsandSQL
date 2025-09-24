7c7fa29f5f0ca8c4c56159fce5d3d3f3c4892fac
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-24 17:35:19 -0500

    01_PHPFundamentals-04_AsociativeArrays

--- 01_phpBasics/c03_arrays.php
+++ 01_phpBasics/c03_arrays.php
@@ -5,6 +5,16 @@
   $weekdays2 = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", true];
 
   // Indexing
-  echo $weekdays[2]; // Outputs "Wednesday"
-  echo $weekdays2[5];
+  echo $weekdays[2] . '<br />'; // Outputs "Wednesday"
+  echo $weekdays2[5] . '<br />';
+
+  // Associative arrays
+  $person = array(
+    "first_name" => "John",
+    "last_name" => "Doe",
+    "age" => 30
+  );
+
+  $person['first_name'] = "Jane"; // Modifying value
+  echo $person["first_name"] . '<br />'; // Outputs "John"
 ?>
