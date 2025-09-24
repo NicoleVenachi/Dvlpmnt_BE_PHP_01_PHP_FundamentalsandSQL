a01b0b2b2d00800135dd6093eb800249791931ab
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-24 17:31:03 -0500

    01_PHPFundamentals-03_Arrays

---
+++ 01_phpBasics/c03_arrays.php
@@ -0,0 +1,10 @@
+<?php 
+  
+  // Declaration and definition
+  $weekdays = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+  $weekdays2 = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", true];
+
+  // Indexing
+  echo $weekdays[2]; // Outputs "Wednesday"
+  echo $weekdays2[5];
+?>
