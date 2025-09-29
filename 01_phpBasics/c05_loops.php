f4f9d9fcdd5f588af520ce4db7d679d5c73ad467
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-29 12:40:22 -0500

    01_PHPFundamentals-04_Loops-LoopiungThorughAndArray

--- 01_phpBasics/c05_loops.php
+++ 01_phpBasics/c05_loops.php
@@ -10,4 +10,16 @@
     echo "The number is: $j <br>";
     $j--;
   }
+
+  // loop through an array
+  $months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  for ($i=0; $i < count($months) ; $i++) { 
+    echo $months[$i] . "<br>";
+  }
+
+  $counter = 0;
+  while ($counter < count($months)) {
+    echo $months[$counter] . "<br>";
+    $counter++;
+  }
 ?>
