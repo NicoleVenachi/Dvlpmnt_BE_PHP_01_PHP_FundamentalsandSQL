961f7871d1edfa7132e2b6f78767c83c738dae2f
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-29 12:26:29 -0500

    01_PHPFundamentals-05_Conditionals-02-Switch

--- 01_phpBasics/c04_conditionals.php
+++ 01_phpBasics/c04_conditionals.php
@@ -1,5 +1,6 @@
 <?php 
 
+  //  ***if-else***
   $age = 20;
   if($age > 18) {
     echo "<h1> User is an adult </h1>";
@@ -7,5 +8,33 @@
     echo "<h1> User is 18 years old </h1>";
   } else {
     echo "<h1> User is a minor </h1> ";
+  }
+
+  // ***switch***
+  $day = 3;
+  switch($day) {
+    case 1:
+      echo "<h2>Monday</h2>";
+      break;
+    case 2:
+      echo "<h2>Tuesday</h2>";
+      break;
+    case 3:
+      echo "<h2>Wednesday</h2>";
+      break;
+    case 4:
+      echo "<h2>Thursday</h2>";
+      break;
+    case 5:
+      echo "<h2>Friday</h2>";
+      break;
+    case 6:
+      echo "<h2>Saturday</h2>";
+      break;
+    case 7:
+      echo "<h2>Sunday</h2>";
+      break;
+    default:
+      echo "<h2>Invalid day</h2>";
   }
 ?>
