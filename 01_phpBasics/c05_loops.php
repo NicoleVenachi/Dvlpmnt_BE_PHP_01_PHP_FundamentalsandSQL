565430b978ca7cc7080ace66b5c7e79d653865fa
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-29 12:37:37 -0500

    01_PHPFundamentals-04_Loops-While

--- 01_phpBasics/c05_loops.php
+++ 01_phpBasics/c05_loops.php
@@ -3,4 +3,11 @@
   for ($i = 1; $i <= 10; $i++) {
     echo "The number is: $i <br>";
   }
+
+  // *** While Loop ***
+  $j = 10;
+  while ($j > 0) {
+    echo "The number is: $j <br>";
+    $j--;
+  }
 ?>
