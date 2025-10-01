1d509fa0c3597570aada6aff39f19f3b7a9dc98b
nicolevenachi <ludwing@unicauca.edu.co>
2025-10-01 13:45:07 -0500

    01_PHPFundamentals-07_Functions-Scope

--- 01_phpBasics/c07_functions.php
+++ 01_phpBasics/c07_functions.php
@@ -10,5 +10,17 @@
     return 0.5 * $base * $height;
   }
   echo triangleArea(10, 5); // Outputs: 25
+
+  // *** Scope of Variables ***
+  $globalVar = "I am global";
+  function testScope() {
+    // echo $globalVar; // Undefined variable error
+    $globalVar = "I am local";
+    echo $globalVar; // Outputs: I am local
+    global $globalVar; // Accessing global variable
+    echo $globalVar;
+  }
+
+  testScope(); // Outputs: I am global
 ?>
 
---
+++ 01_phpBasics/c09_mathFunctions.php
@@ -0,0 +1,11 @@
+<?php 
+  $number = 5.7;
+  echo round($number) . "<br>"; // 6
+
+  echo rand(1, 10) . "<br>"; // Random number between 1 and 10
+
+  echo ceil(4.3) . "<br>"; // 5
+  echo floor(4.7) . "<br>"; // 4
+
+  echo M_PI . "<br>"; // 3.141592653589793
+?>
