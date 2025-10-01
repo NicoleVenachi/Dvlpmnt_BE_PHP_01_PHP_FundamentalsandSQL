4371e51d8cc9c0051b6a82b893fbb1ee9ed5ef9a
nicolevenachi <ludwing@unicauca.edu.co>
2025-10-01 14:20:06 -0500

    01_PHPFundamentals-09_Php7_SpaceShipOperator

--- 01_phpBasics/c11_php7.php
+++ 01_phpBasics/c11_php7.php
@@ -13,4 +13,17 @@
   // $num = '4';
   $num = 4;
   echo square($num); // Outputs: 16
+
+  // *** Spaceship Operator ***
+  // returns -1, 0, or 1 when $a is respectively less than, equal to, or greater than $b.
+  echo 5 <=> 10; // Outputs: -1
+
+  $nums = [1, 3, 2, 5, 4];
+  echo implode(", ", $nums); // "1, 3, 2, 5, 4"
+  // sort($nums)
+  function customSort($a, $b) {
+    return $a <=> $b; // Ascending order
+  }
+  usort($nums, 'customSort');
+  echo implode(", ", $nums); // "1, 2, 3, 4, 5"
 ?>
