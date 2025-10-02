bbc70f90a86d0c8f53d3717d93c935d554cc13f1
nicolevenachi <ludwing@unicauca.edu.co>
2025-10-02 15:30:23 -0500

    02_Forms_06-FormValidation

--- 02_formularies/c02_form.php
+++ 02_formularies/c02_form.php
@@ -1,18 +1,56 @@
+<?php 
+  $errors = [];
+  if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    extract($_GET);
+  } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    extract($_POST);
+  }
+  if(isset($submit) && !empty($name) && !empty($email)) {
+    // cleaning input data
+    $name = filter_var($name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
+    $email = htmlspecialchars(strip_tags(trim($email)));
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    echo "Form submitted<br>";
+    echo "Name: $name <br>";
+    echo "Email: $email <br>";
+  } elseif (isset($submit)) {
+    if (empty($name)) array_push($errors, "Name required");
+    if (empty($email)) {
+      array_push($errors, "Email required");
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) array_push($errors, "Wrong Email");
+    } ;
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Forms</title>
+  <style>
+    .errors-list{
+      color: red
+    }
+  </style>
 </head>
 <body>
-
-  <form action="inputValidation.php" method="get">
+  <form action="<?php echo(htmlspecialchars($_SERVER['PHP_SELF']))?>" method="get">
     <input type="text" name="name" placeholder="Type your name">
     <br>
     <input type="email" name="email" placeholder="Type your email">
     <br>
     <input type="submit" value="Send" name="submit">
   </form>
+  <? php if(!empty($errors)):?>
+    <ul class='errors-list'>
+      <?php 
+        foreach($errors as $error) {
+          echo("<li>$error</li>");
+        }
+      ?>
+    </ul>
+  <? php endif; ?>
 </body>
 </html>
