cb2cc10cff98caa535527f939a31a0c0d34b5d17
nicolevenachi <ludwing@unicauca.edu.co>
2025-10-03 12:56:26 -0500

    02_Forms_08-ContactFormPractice-InputsValidations

--- practices/contactForm/index.php
+++ practices/contactForm/index.php
@@ -1,4 +1,32 @@
-<?php 
+<?php
 
-  include('index.view.php')
+  $errors = array();
+  $sent = false;
+
+  if (!isset($_POST["submit"])) die();
+  extract($_POST);
+
+  // cleaning input data
+  if (!empty($name)) {
+    $name = filter_var(trim($name), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  } else {
+    array_push($errors, "Please add a valid name");
+  }
+
+  if (!empty($email)) {
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = htmlspecialchars(strip_tags(trim($email)));
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) array_push($errors, "Wrong Email");
+  } else {
+    array_push($errors, "Please add a valid email");
+  }
+
+  if (!empty($message)) {
+  // $message = filter_var($message, FILTER_SANITIZE_FULL_
+  } else {
+  }
+
+
+  require_once('index.view.php');
+
 ?>
--- practices/contactForm/index.view.php
+++ practices/contactForm/index.view.php
@@ -1,32 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Bitcount+Prop+Single+Ink:wght@100..900&family=Bitcount+Prop+Single:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Bitcount+Prop+Single+Ink:wght@100..900&family=Bitcount+Prop+Single:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
   <title>Contact form</title>
 </head>
+
 <body>
   <div class="wrap">
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
 
       <input type="text" class="form-control" name="name" value="" placeholder="Name:">
-      <input type="email" class="form-control" name="name" value="" placeholder="Email:">
+      <input type="email" class="form-control" name="email" value="" placeholder="Email:">
 
       <textarea name="message" id="message" class="form-control" placeholder="Message....."></textarea>
 
-      <!-- <div class="alert error"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit explicabo officia dolores assumenda, velit vero temporibus vel voluptatum eum et hic debitis dolore dolorum, a maxime ipsa consequuntur deleniti perspiciatis.        
-      </div>
-
-      <div class="alert success">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati at, ipsa quos ratione, facilis accusamus voluptatum sapiente a nisi dolorem quasi sequi. Unde vero iure doloremque nisi nostrum tenetur quia?
-      </div> -->
+      <?php if (!empty($errors)): ?>
+        <div class="alert error">
+          <?php
+          foreach ($errors as $error) {
+            echo htmlspecialchars($error) . '<br>';
+          }
+          ?>
+        </div>
+      <?php elseif($sent): ?>
+        <div class="alert success">
+          <p>Succesfully sent</p>
+        </div>
+      <?php endif ?>
       <input type="submit" name="submit" value="Send Email" class="btn btn-primary">
     </form>
 
   </div>
 </body>
+
 </html>
