2d2c35625de1898ff52a86747cf7b3f767ac7534
nicolevenachi <ludwing@unicauca.edu.co>
2025-10-07 19:52:17 -0500

    05_MySQL_01-DB_Creation

---
+++ 05_MySQL/01_DB_Creation.sql
@@ -0,0 +1,7 @@
+-- --- DB CREATION -----
+-- DROP TABLE users
+-- CREATE TABLE users (id INT AUTO_INCREMENT, name varchar(100) NOT NULL, email varchar(100) NOT NULL, PRIMARY KEY (id))
+
+-- SHOW tables;
+describe users;
+SELECT * FROM users;
