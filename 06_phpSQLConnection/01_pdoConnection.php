1e548a22913144341d2f465d0778b42705acc4bf
Nicolr Venachi <ludwing@unicauca.edu.co>
2025-10-29 12:53:28 -0500

    06_PD0_02->QuerysUsingQueryMethod

--- 06_phpSQLConnection/01_pdoConnection.php
+++ 06_phpSQLConnection/01_pdoConnection.php
@@ -1,5 +1,7 @@
 <?php 
 
+  // $id = $_GET["id"];
+  $id = '2 or id=4'; // Query Injection example
   try {
     // Database connection parameters
     $dbtype = 'mysql';
@@ -10,7 +12,17 @@
     $username = "root";
     $password = "";
     $connection = new PDO($dsn, $username, $password);
-    
+
+    // Query method: exec() - for SELECT, INSERT, UPDATE, DELETE
+    // $queryResults = $connection -> query("INSERT INTO users (id, name, email) VALUES(null, 'Juana', 'juana@gmail.com')");
+
+    $queryResults = $connection -> query("SELECT * FROM users WHERE id = $id");
+    foreach ($queryResults as $row) {
+      // print_r($row);
+      echo $row["name"] . ' - ' . $row['email'] .  '<br />';
+    }
+
+
   } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
   }
