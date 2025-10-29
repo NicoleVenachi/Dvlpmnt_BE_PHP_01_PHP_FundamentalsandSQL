73325db01ae6faae107bf59fb20f35bc985896f9
Nicolr Venachi <ludwing@unicauca.edu.co>
2025-10-29 13:45:34 -0500

    06_PD0_03->PreparedStatemens

--- 06_phpSQLConnection/01_pdoConnection.php
+++ 06_phpSQLConnection/01_pdoConnection.php
@@ -1,7 +1,7 @@
 <?php 
 
   // $id = $_GET["id"];
-  $id = '2 or id=4'; // Query Injection example
+  $id = '3'; // Query Injection example
   try {
     // Database connection parameters
     $dbtype = 'mysql';
@@ -16,11 +16,29 @@
     // Query method: exec() - for SELECT, INSERT, UPDATE, DELETE
     // $queryResults = $connection -> query("INSERT INTO users (id, name, email) VALUES(null, 'Juana', 'juana@gmail.com')");
 
-    $queryResults = $connection -> query("SELECT * FROM users WHERE id = $id");
-    foreach ($queryResults as $row) {
-      // print_r($row);
+    // $queryResults = $connection -> query("SELECT * FROM users WHERE id = $id");
+    // foreach ($queryResults as $row) {
+    //   // print_r($row);
+    //   echo $row["name"] . ' - ' . $row['email'] .  '<br />';
+    // }
+
+    // Prepared Statements to prevent SQL Injection
+    $statement = $connection->prepare("SELECT * FROM users WHERE id = :id OR name = :name");
+    
+    $statement->execute([
+      ":id" => $id,
+      ":name"=> 'Carlos',
+    ]);
+    
+    $results = $statement->fetchAll();
+
+    // print_r($results);
+    // $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($results as $row) {
       echo $row["name"] . ' - ' . $row['email'] .  '<br />';
     }
+
 
 
   } catch (PDOException $e) {
