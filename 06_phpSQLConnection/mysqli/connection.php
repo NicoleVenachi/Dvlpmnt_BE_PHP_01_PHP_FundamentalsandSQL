0a32b89a940057f64e3d22ef4c6463e2e1ac124e
Nicolr Venachi <ludwing@unicauca.edu.co>
2025-10-30 12:51:04 -0500

    07_Mysqli_02->Queries

--- 06_phpSQLConnection/mysqli/connection.php
+++ 06_phpSQLConnection/mysqli/connection.php
@@ -7,5 +7,23 @@
     die("Sorry, inner server error". $connection->connect_error); 
   }; // Should print 0 if connection is successful
 
-  
+  // Simple Query Execution
+  // $sqlQuery = 'SELECT * FROM users LIMIT 3';
+  $id = isset($_GET["id"]) ? $_GET["id"]: 1;
+  $sqlQuery = "SELECT * FROM users WHERE id = $id"; // Query Injection example
+  $results = $connection->query($sqlQuery);
+  if ($results->num_rows > 0) {
+    // echo $results->num_rows;
+    // echo '<pre>';
+    // var_dump($results -> fetch_assoc()); // Fetch a single row as an associative array
+    // echo '</pre>';
+
+    // echo $results -> fetch_assoc()['name']; // Access specific column value
+
+    while ($row = $results -> fetch_assoc()) {
+      echo $row['name'] . ' - ' . $row['email'] . '<br />';
+    }
+  } 
+
+
 ?>
